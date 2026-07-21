7bf7ed16c488d18f872cef8fc97f1b6f03eb52bb
estebanortenzi <esteban_ortenzi@hotmail.com>
2026-07-21 00:15:27 -0300

    style: update header brand and main heading text

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -28,7 +28,7 @@ export function SiteHeader() {
     <header className="site-header">
       <div className="site-header__inner">
         <Link href="/" className="brand">
-          Guárdamela
+          Guardamela
         </Link>
         <nav className="site-nav">
           <Link href="/">Explorar</Link>
